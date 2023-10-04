61d1d5643a25d701f0b9c2d0249a1564b0728056
KayChang <kaykwing@gmail.com>
2023-10-04 16:18:39 +0800

    add: create framework project and demo project

---
+++ YTPlayer/YTPlayer/YTPlayer.h
@@ -0,0 +1,18 @@
+//
+//  YTPlayer.h
+//  YTPlayer
+//
+//  Created by Kay Chang on 2023/10/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YTPlayer.
+FOUNDATION_EXPORT double YTPlayerVersionNumber;
+
+//! Project version string for YTPlayer.
+FOUNDATION_EXPORT const unsigned char YTPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YTPlayer/PublicHeader.h>
+
+
